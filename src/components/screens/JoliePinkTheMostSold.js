4b20fff361af73ef1f691588135a9c935830cb9c
KarlaArauz <61438719+KarlaArauz@users.noreply.github.com>
2021-04-13 17:36:17 -0600

    modificacion de la pantalla lo mas vendido

--- src/components/screens/JoliePinkTheMostSold.js
+++ src/components/screens/JoliePinkTheMostSold.js
@@ -1,166 +1,13 @@
 import React, {useContext} from "react";
-import {StyleSheet, View, Text, Dimensions, Image, TouchableOpacity} from "react-native";
+import {StyleSheet, View, Text, Dimensions, Image, TouchableOpacity, FlatList} from "react-native";
 import {Header, Card} from 'react-native-elements';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import LoMasVendido from "../shared/LoMasVendido";
 
 const {width, height} = Dimensions.get("window");
 
-const JoliePinkTheMostSold = () =>{
+const JoliePinkTheMostSold = ({navigation}) =>{
 
-    const LoMasVendido=[
-        {
-            "id": 2,
-            "nombre": "Bikini dos piezas con hojas",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "600",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/G3Y2B59/bikini2.jpg"
-          },
-          {
-            "id": 8,
-            "nombre": "Bikini abdomen descubierto diagonal",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "800",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/wpnSrWp/bikini1.jpg"
-          },
-          {
-            "id": 9,
-            "nombre": "Bikini dos piezas agarre a un hombro",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "1000",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/vhFqH2m/bikini2.jpg"
-          },
-        {
-            "id": 1,
-            "nombre": "Blusa brillante espalda descubierta",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "800",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/NnSn1V3/Blusas1.jpg"
-          },
-          {
-            "id": 8,
-            "nombre": "Blusa manga al codo hombros descubiertos",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "700",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/NNF5zMD/blusa1.jpg"
-          },
-          {
-            "id": 11,
-            "nombre": "Blusa floreada mangas largas al hombro",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "800",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/37vScgp/blusa4.jpg"
-          },
-        {
-            "id": 1,
-            "nombre": "Bolso con agarradera pequeña y cadena",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "800",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/DWsgTp5/Bolso1.jpg"
-          },
-          {
-            "id": 8,
-            "nombre": "Diademas delagadas con perlas",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "400",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/9r76GbV/accesorio1.jpg"
-          },
-          {
-            "id": 10,
-            "nombre": "Pasadores de oceano bañados en oro ",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "1000",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/M7wKWjJ/accesorio3.jpg"
-          },
-        {
-            "id": 1,
-            "nombre": "Chaqueta estilo artesanal semiformal",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "800",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/vmvTGsp/Chaquetas1.jpg"
-          },
-          {
-            "id": 4,
-            "nombre": "Chaqueta rayada con estilo artesanal en borde",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "900",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/KhjyCPh/Chaquetas4.jpg"
-          },
-          {
-            "id": 11,
-            "nombre": "Chaqueta de cuero casual",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "1000",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/2jX0NB3/chaqueta4.jpg"
-          },
-        {
-            "id": 7,
-            "nombre": "Pantalon estilo culotte largo a la cintura",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "1000",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/4NKc5x4/Pantalon7.jpg"
-          },
-          {
-            "id": 8,
-            "nombre": "Jean a la cintura",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "800",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/mNT5sv1/pantalon1.jpg"
-          },
-          {
-            "id": 12,
-            "nombre": "Pantalo con cadena casual",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "900",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/9cCgvvy/pantalon5.jpg"
-          },
-        {
-            "id": 10,
-            "nombre": "Vestido de seda corto ceñido al cuerpo con agarres",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "1100",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/pXdT2yb/vestido3.jpg"
-          },
-          {
-            "id": 11,
-            "nombre": "Vestido largo ceñido al cuerpo corte en la pierna",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "1200",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/68gp0pS/vestido4.jpg"
-          },
-          {
-            "id": 12,
-            "nombre": "Vestido medio ceñido al cuerpo corte en la pierna",
-            "talla": ["xs","s","m","l","xl"],
-            "precio": "1200",
-            "color":["rosa","blanco","negro","amarillo"],
-            "imagen": "https://i.ibb.co/BP39kq1/vestido5.jpg"
-          },
-    ]
- //   return(
- //       <SafeAreaProvider>
- //           <Header
- //            centerComponent={{ text: 'Lo mas vendido', style: { color: '#f9ece9', fontSize: 25 }}}
- //            containerStyle = { {backgroundColor : '#bd787d' , justifyContent : 'space-around'} }
- //           />
- //       </SafeAreaProvider>
- //   )
  return(
     <FlatList
     data = {LoMasVendido}
@@ -176,7 +23,9 @@ const JoliePinkTheMostSold = () =>{
                                         style ={styles.image}/>
                                         <Text>{}</Text>
                                         <View style= {styles.contenedorInformacion}>
-                                            <View><Text>{item.nombre}</Text></View>    
+                                            <View style={styles.contenedorNombre}>
+                                              <Text>{item.nombre}</Text>
+                                            </View>    
                                             <Text>{}</Text>
                                             <Text>Precio: L.{item.precio}</Text> 
                                             <Text>{}</Text>
@@ -278,6 +127,13 @@ const styles = StyleSheet.create({
     },
     botonCompra:{
         marginTop: 10
+    }, 
+    contenedorNombre:{
+      marginTop: 10,
+      width: width * 0.40
+    },
+    nombre:{
+      fontSize: 16
     }
 
 });
