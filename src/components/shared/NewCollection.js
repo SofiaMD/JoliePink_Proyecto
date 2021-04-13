31366730f1cdef3deee650041b178ac79b49b0ef
KarlaArauz <61438719+KarlaArauz@users.noreply.github.com>
2021-04-13 17:39:04 -0600

    crear un componente para la data de nueva coleccion

---
+++ src/components/shared/NewCollection.js
@@ -0,0 +1,56 @@
+export default NuevaColeccion = [
+    {
+        imgUrl: "https://i.ibb.co/9YdRtXG/outfit1.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/xgPcV9H/outfit2.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/vBwsRXx/outfit3.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/sH6ZNFR/outfit4.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/8NmZ3dW/outfit5.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/FBDjpWT/outfit6.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/x2FWng8/outfit7.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/WnTWWjM/outfit8.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/VHyrtfv/outfit9.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/sshRCyF/outfit10.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/tzYT9yq/outfit11.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/NsN6RQ2/outfit12.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/p3BYrk5/outfit13.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/swFChQ5/outfit14.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/ynxzZ2T/outfit15.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/kq497SJ/outfit16.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/Jq3fsNH/outfit17.jpg"
+    },
+    {
+        imgUrl: "https://i.ibb.co/QcJrMmg/outfit18.jpg"
+    },
+]
